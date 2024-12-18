0308e3cc799433a4592885edf0e6eac945811f2b
yungx666 <yungx21sxx@gmail.com>
2024-12-18 18:18:45 +0300

    Фотки

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,21 @@ const ONE_DAY = 60 * 60 * 24 * 1000;
 const ONE_WEEK = ONE_DAY * 7;
 
 export default defineNuxtConfig({
+	compatibilityDate: '2024-12-18',
 	runtimeConfig: {
 		cookieName: process.env.COOKIE_NAME || "__session",
 		cookieSecret: process.env.COOKIE_SECRET || "secret",
 		cookieExpires: parseInt(process.env.COOKIE_REMEMBER_ME_EXPIRES || ONE_DAY.toString(), 10), // 1 day
 		cookieRememberMeExpires: parseInt(process.env.COOKIE_REMEMBER_ME_EXPIRES || ONE_WEEK.toString(), 10), // 7 days
+	},
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@use "@/assets/_var.scss" as *;',
+				}
+			}
+		},
 	},
 	devtools: {
 		enabled: true,
