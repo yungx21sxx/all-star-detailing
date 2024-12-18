744b842fcf0bc403993657a0ab1a54e36c64a79c
yungx666 <yungx21sxx@gmail.com>
2024-12-18 21:02:30 +0300

    Фотки

--- composables/UseTelegram.ts
+++ composables/UseTelegram.ts
@@ -1,17 +1,17 @@
 export default () => {
-	const token = '6193438455:AAG9HZ3kPfSEdT73gboU4X_Dokpk-h8NTvk\n'
-	const chatID = -1001746507788
+	const token = '7161194033:AAGBmx7I_ZipqmwAdS2SUsSwFYoCyGjS5jc';
+	const chatID = -1002495293643;
 	const getUrl = (text: string): string => `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatID}&text=${text}`
 	// @ts-ignore
 	const fetchForCallData = async ({phone, name}) => {
-		const text = `Клиент оставил заявку: %0AИмя: ${name} %0AНомер телефона: ${phone}`
+		const text = `Клиент оставил заявку: %0A👶Имя: ${name} %0A📱Номер телефона: 8 ${phone}`
 		const url = getUrl(text)
 		return useFetch(url)
 	}
 
 	// @ts-ignore
 	const fetchForRegistrationData = async ({date, phone, name, chosenService}) => {
-		const text = `🚗Клиент записался на услугу🚗: ${chosenService} %0A👶Имя: ${name} %0A📱Номер телефона: +7 ${phone} %0A📅Хочет записаться на: ${date}`
+		const text = `Клиент записался на услугу:%0A🚗 ${chosenService} %0A👶Имя: ${name} %0A📱Номер телефона: 8 ${phone} %0A📅Хочет записаться на: ${date}`
 		const url = getUrl(text)
 		return useFetch(url)
 	}
